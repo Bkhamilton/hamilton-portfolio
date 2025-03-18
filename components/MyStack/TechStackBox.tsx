ba53362617b20384041a2c7c4ee0d4d123e8a74b
Bkhamilton <bkhamilton310@gmail.com>
2025-03-18 17:09:22 -0400

    added logos for tech stack components

--- components/MyStack/TechStackBox.tsx
+++ components/MyStack/TechStackBox.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { SiJavascript, SiTypescript, SiReact, SiPython } from "react-icons/si";
+import { SiNextdotjs, SiTailwindcss, SiCss3, SiExpress, SiGraphql } from "react-icons/si";
+import { SiPostgresql, SiFirebase, SiContentful, SiTableau } from "react-icons/si";
+import { TbBrandReactNative } from "react-icons/tb";
+import { IoIosGitBranch } from "react-icons/io";
+import { TiHtml5 } from "react-icons/ti";
+import { FaNode } from "react-icons/fa";
+import { DiMongodb, DiMysql } from "react-icons/di";
+import { FaAws } from "react-icons/fa";
 
 interface TechStackBoxProps {
     type: string;
     technologies: string[];
 }
+
+const logoMapping: { [key: string]: JSX.Element } = {
+    "JavaScript": <SiJavascript className="text-yellow-500" />,
+    "TypeScript": <SiTypescript className="text-blue-500" />,
+    "React": <SiReact className="text-blue-600" />,
+    "React Native": <TbBrandReactNative className="text-blue-600" />,
+    "Python": <SiPython className="text-yellow-500" />,
+    "Next.js": <SiNextdotjs className="text-white" />,
+    "Tailwind": <SiTailwindcss className="text-blue-400" />,
+    "CSS": <SiCss3 className="text-blue-500" />,
+    "HTML": <TiHtml5 className="text-orange-500" />,
+    "Node.js": <FaNode className="text-green-500" />,
+    "Express": <SiExpress className="text-gray-800" />,
+    "GraphQL": <SiGraphql className="text-purple-600" />,
+    "MongoDB": <DiMongodb className="text-green-600" />,
+    "PostgreSQL": <SiPostgresql className="text-blue-700" />,
+    "MySQL": <DiMysql className="text-blue-600" />,
+    "Firebase": <SiFirebase className="text-yellow-500" />,
+    "Contentful CMS": <SiContentful className="text-blue-600" />,
+    "Git": <IoIosGitBranch className="text-orange-500" />,
+    "AWS": <FaAws className="text-orange-500" />,
+    "Tableau": <SiTableau className="text-blue-500" />,
+};
 
 const TechStackBox: React.FC<TechStackBoxProps> = ({ type, technologies }) => {
     // Helper function to split the technologies array into chunks of 6
@@ -26,7 +58,18 @@ const TechStackBox: React.FC<TechStackBoxProps> = ({ type, technologies }) => {
                 {technologyChunks.map((chunk, columnIndex) => (
                     <ul key={columnIndex} className="space-y-2 w-1/2">
                         {chunk.map((tech, index) => (
-                            <li key={index} className="text-xl">{tech}</li>
+                            <li
+                                key={index}
+                                className="text-xl flex items-center space-x-2"
+                            >
+                                {/* Render the logo if it exists in logoMapping */}
+                                {logoMapping[tech] && (
+                                    <span className="flex items-center justify-center">
+                                        {logoMapping[tech] as React.ReactNode}
+                                    </span>
+                                )}
+                                <span>{tech}</span>
+                            </li>
                         ))}
                     </ul>
                 ))}

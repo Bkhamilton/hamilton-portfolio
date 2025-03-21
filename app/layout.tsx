a1ca83791b4d58374952c12de2f84b71ec386347
Bkhamilton <bkhamilton310@gmail.com>
2025-03-20 22:35:56 -0400

    removed unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StarsBackground from '@/components/Helpers/StarsBackground'
-import ParallaxEffects from "@/components/Helpers/ParallaxEffect";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
